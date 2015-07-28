d4f9d9b0ce3a837e735d1d119654abfbc284f314
Xiao-Feng Xie <xie@wiomax.com>
2015-07-28 07:17:53 -0400

    update binary file

--- myprojects/examples.bat
+++ myprojects/examples.bat
@@ -1,1 +1,1 @@
-java -server -Xmx256M -cp ../binary/MAOS_SEQ.jar maosKernel.MAOSExecuter TSP:Problem=rl1323 N=300 T=100 DUP_TIMES=5 solver=STD_PRB_3Opt_REAX+SEAX:isAlpha=true
+java -server -Xmx256M -jar ../binary/MAOS_SEQ.jar TSP:Problem=rl1323 N=300 T=100 DUP_TIMES=5 solver=STD_PRB_3Opt_REAX+SEAX:isAlpha=true
