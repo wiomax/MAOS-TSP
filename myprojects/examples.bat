308a6b099b5e77f99ee435baa7c92e3761de6368
WIOMAX Software <wiomax@users.noreply.github.com>
2019-08-07 16:16:17 -0400

    Update examples.bat

--- myprojects/examples.bat
+++ myprojects/examples.bat
@@ -1,2 +1,2 @@
-java -server -Xmx256M -jar ../release/MAOS_SEQ.jar TSP:Problem=rl1323 N=300 T=100 DUP_TIMES=5 solver=STD_PRB_3Opt_REAX+SEAX:isAlpha=true
+java -server -Xmx256M -cp ../binary/MAOS_SEQ.jar maosKernel.MAOSExecuter TSP:Problem=rl1323 N=300 T=100 DUP_TIMES=5 solver=STD_PRB_3Opt_REAX+SEAX:isAlpha=true
 java -server -Xmx256M -cp ../binary/MAOS_SEQ.jar maosKernel.MAOSExecuter TSP:Problem=a280   N=50
